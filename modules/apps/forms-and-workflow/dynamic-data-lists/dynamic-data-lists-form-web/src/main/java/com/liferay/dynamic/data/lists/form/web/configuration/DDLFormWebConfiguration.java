3feac7f628a4c35189bc7679b3857c1c1f652c17
Lance Ji <zheyi.ji@liferay.com>
2017-10-17 14:20:58 -0700

    LPS-75293 Fixed localization for forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/configuration/DDLFormWebConfiguration.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/configuration/DDLFormWebConfiguration.java
@@ -32,7 +32,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 public interface DDLFormWebConfiguration {
 
 	@Meta.AD(
-		deflt = "descriptive", optionLabels = {"Descriptive", "List"},
+		deflt = "descriptive", name = "default-display-view",
 		optionValues = {"descriptive", "list"}, required = false
 	)
 	public String defaultDisplayView();
