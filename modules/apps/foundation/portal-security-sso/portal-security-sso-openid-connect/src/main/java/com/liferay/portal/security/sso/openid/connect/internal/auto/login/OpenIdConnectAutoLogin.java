79ab23ac400e2bb9cf192be76e80a113c775b5f2
Lance Ji <zheyi.ji@liferay.com>
2018-01-10 15:49:59 -0800

    LPS-76963 Apply to usages

--- modules/apps/foundation/portal-security-sso/portal-security-sso-openid-connect/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/auto/login/OpenIdConnectAutoLogin.java
+++ modules/apps/foundation/portal-security-sso/portal-security-sso-openid-connect/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/auto/login/OpenIdConnectAutoLogin.java
@@ -20,8 +20,8 @@ import com.liferay.portal.kernel.security.auto.login.BaseAutoLogin;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.security.sso.openid.connect.OpenIdConnect;
-import com.liferay.portal.security.sso.openid.connect.OpenIdConnectFlowState;
-import com.liferay.portal.security.sso.openid.connect.OpenIdConnectSession;
+import com.liferay.portal.security.sso.openid.connect.internal.OpenIdConnectFlowState;
+import com.liferay.portal.security.sso.openid.connect.internal.OpenIdConnectSession;
 import com.liferay.portal.security.sso.openid.connect.constants.OpenIdConnectWebKeys;
 
 import javax.servlet.http.HttpServletRequest;
