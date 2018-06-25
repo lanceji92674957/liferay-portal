44deecde0d5299ef65f41f5a3e1772f135c7e648
Lance Ji <zheyi.ji@liferay.com>
2018-06-25 13:22:43 -0700

    LPS-82509 Create version-classloader pair to keep record of classloaders of different versions

--- modules/core/petra/petra-lang/src/main/java/com/liferay/petra/lang/ClassLoaderPool.java
+++ modules/core/petra/petra-lang/src/main/java/com/liferay/petra/lang/ClassLoaderPool.java
@@ -107,4 +107,24 @@ public class ClassLoaderPool {
 		register("GlobalClassLoader", ClassLoaderPool.class.getClassLoader());
 	}
 
+	private static class VersionedClassLoader {
+
+		public VersionedClassLoader(String version, ClassLoader classLoader) {
+			_version = version;
+			_classLoader = classLoader;
+		}
+
+		public ClassLoader getClassLoader() {
+			return _classLoader;
+		}
+
+		public String getVersion() {
+			return _version;
+		}
+
+		private final ClassLoader _classLoader;
+		private final String _version;
+
+	}
+
 }
