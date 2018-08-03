3188816edbfb2fe1486c8ea4d94dbd094bc9c220
Lance Ji <zheyi.ji@liferay.com>
2018-08-02 18:11:53 -0700

    LPS-83855 Copy original logic of toString and encode from UnicodeProperties to new util class

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropertiesEncoderUtil.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author Lance Ji
+ */
+public class PropertiesEncoderUtil {
+
+	public String toString() {
+		StringBundler sb = new StringBundler(4 * size());
+
+		Map<String, String> treeMap = new TreeMap<>(this);
+
+		for (Map.Entry<String, String> entry : treeMap.entrySet()) {
+			String value = entry.getValue();
+
+			if (Validator.isNull(value)) {
+				continue;
+			}
+
+			if (_safe) {
+				value = _encode(value);
+			}
+
+			sb.append(entry.getKey());
+			sb.append(StringPool.EQUAL);
+			sb.append(value);
+			sb.append(StringPool.NEW_LINE);
+		}
+
+		return sb.toString();
+	}
+
+	private static String _encode(String value) {
+		String encodedValue = StringUtil.replace(
+			value, StringPool.RETURN_NEW_LINE, _SAFE_NEWLINE_CHARACTER);
+
+		return StringUtil.replace(
+			encodedValue, new char[] {CharPool.NEW_LINE, CharPool.RETURN},
+			new String[] {_SAFE_NEWLINE_CHARACTER, _SAFE_NEWLINE_CHARACTER});
+	}
+
+	private static final String _SAFE_NEWLINE_CHARACTER =
+		"_SAFE_NEWLINE_CHARACTER_";
+
+}
