0b397271459e1d992780905b266c8208e66e485c
Lance Ji <zheyi.ji@liferay.com>
2018-09-14 01:28:26 -0700

    LPS-85008 Mock helper setup

---
+++ portal-test/src/com/liferay/portal/kernel/test/util/MockHelperUtil.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.test.util;
+
+import com.liferay.portal.kernel.util.ProxyUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Lance Ji
+ */
+public class MockHelperUtil {
+
+	public static <T> T initMock(Class<T> clazz) {
+		return (T)ProxyUtil.newProxyInstance(
+			clazz.getClassLoader(), new Class<?>[] {clazz},
+			new MockInvocationHandler(clazz));
+	}
+
+	public static <T> T setMethodAlwaysReturnExpected(
+			Class<T> clazz, String methodName, Object expectedResult,
+			Class<?>... parameterTypes)
+		throws Exception {
+
+		T mock = initMock(clazz);
+
+		setMethodAlwaysReturnExpected(
+			mock, methodName, expectedResult, parameterTypes);
+
+		return mock;
+	}
+
+	public static <T, R> void setMethodAlwaysReturnExpected(
+			T targetMock, String methodName, R expectedResult,
+			Class<?>... parameterTypes)
+		throws Exception {
+
+		InvocationHandler invocationHandler = ProxyUtil.getInvocationHandler(
+			targetMock);
+
+		if (invocationHandler instanceof MockInvocationHandler) {
+			MockInvocationHandler mockInvocationHandler =
+				(MockInvocationHandler)invocationHandler;
+
+			mockInvocationHandler._registerMethodReturnExpected(
+				methodName, expectedResult, parameterTypes);
+		}
+		else {
+			throw new UnsupportedOperationException("Not a valid mock");
+		}
+	}
+
+	private static class MethodInvocationHandler implements InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+
+			return _expectedResult;
+		}
+
+		private MethodInvocationHandler(Object expectedResult) {
+			_expectedResult = expectedResult;
+		}
+
+		private void _setExpectedResult(Object expectedResult) {
+			_expectedResult = expectedResult;
+		}
+
+		private Object _expectedResult;
+
+	}
+
+	private static class MockInvocationHandler implements InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+
+			InvocationHandler invocationHandler = _invocationHandlers.get(
+				method);
+
+			if (invocationHandler == null) {
+				Class<?> returnType = method.getReturnType();
+
+				if (_defaultValue.get(returnType) != null) {
+					return _defaultValue.get(returnType);
+				}
+
+				return null;
+			}
+
+			return invocationHandler.invoke(proxy, method, args);
+		}
+
+		private MockInvocationHandler(Class<?> clazz) {
+			_clazz = clazz;
+			_invocationHandlers = new HashMap<>();
+		}
+
+		private void _registerMethodReturnExpected(
+				String methodName, Object expectedResult,
+				Class<?>... parameterTypes)
+			throws Exception {
+
+			Method method = _clazz.getMethod(methodName, parameterTypes);
+
+			MethodInvocationHandler methodInvocationHandler =
+				_invocationHandlers.get(method);
+
+			if (methodInvocationHandler == null) {
+				methodInvocationHandler = new MethodInvocationHandler(
+					expectedResult);
+
+				_invocationHandlers.put(method, methodInvocationHandler);
+			}
+
+			methodInvocationHandler._setExpectedResult(expectedResult);
+		}
+
+		private static final Map<Class<?>, Object> _defaultValue =
+			new HashMap<Class<?>, Object>() {
+				{
+					_defaultValue.put(int.class, 0);
+					_defaultValue.put(Integer.class, 0);
+					_defaultValue.put(long.class, 0L);
+					_defaultValue.put(Long.class, 0L);
+					_defaultValue.put(Boolean.class, false);
+					_defaultValue.put(boolean.class, false);
+					_defaultValue.put(String.class, "");
+				}
+			};
+
+		private final Class<?> _clazz;
+		private final Map<Method, MethodInvocationHandler> _invocationHandlers;
+
+	}
+
+}
